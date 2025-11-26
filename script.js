59783a617d0a181b3a198a920c166b96ad5da943
rutujaa121 <rutujaaugale@gmail.com>
2025-11-25 19:53:29 -0800

    added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const wireless = document.querySelector('.wireless');
 const bulb = document.querySelector('.bulb');
 const bulbButton = document.querySelector('.bulb_button');
-let isOn = false;
+
 
 wireless.addEventListener('click', () => {
     isOn = !isOn;
