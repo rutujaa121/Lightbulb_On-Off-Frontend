db914403fd24bd3b5d5d271972ca6a49d2e8e970
rutujaa121 <rutujaaugale@gmail.com>
2025-12-01 20:56:51 -0800

    added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const wire = document.querySelector('wire');
+wire.addEventListener('click', () => {
+    wire.classList.toggle('active');
+});
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    alert('Button clicked!');
+});
