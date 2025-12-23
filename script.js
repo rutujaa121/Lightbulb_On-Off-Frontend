a56b5a0f26ba788308bcb3b2ff1bb6895014c542
rutujaa121 <rutujaaugale@gmail.com>
2025-12-23 07:58:11 -0800

    added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const wire = document.querySelector('wire');
-wire.addEventListener('click', () => {
-    wire.classList.toggle('active');
-});
-
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    alert('Button clicked!');
-});
+// const wire = document.querySelector('wire');
+// wire.addEventListener('click', () => {
+//     wire.classList.toggle('active');
+// });
+// 
+// const button = document.querySelector('button');
+// button.addEventListener('click', () => {
+//     alert('Button clicked!');
+// });
